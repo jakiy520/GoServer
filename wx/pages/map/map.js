addcf4ab9bff1c25bd7b6af30a316189c183b456
wangdanfeng <390969468@qq.com>
2018-08-01 17:38:06 +0800

    wx地图开发

--- wx/pages/map/map.js
+++ wx/pages/map/map.js
@@ -1,11 +1,11 @@
 Page({
   data: {
-    latitude:  28.642557,
-    longitude: 121.471452,
+    latitude: 28.643813,
+    longitude: 121.468869,
     markers: [{
       id: 1,
-      latitude: 28.642557,
-      longitude: 121.471452,
+      latitude: 28.643813,
+      longitude: 121.468869,
       name: '如果果业'
     }]
     
