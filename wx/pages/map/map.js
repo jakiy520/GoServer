3c76e9bab6275087c6eed05bc432374b0527025b
wangdanfeng <390969468@qq.com>
2018-08-02 21:44:51 +0800

    新增联系地图

--- wx/pages/map/map.js
+++ wx/pages/map/map.js
@@ -12,5 +12,27 @@ Page({
   },
   onReady: function (e) {
     this.mapCtx = wx.createMapContext('myMap')
+  },
+  calling1:function(){
+    wx.makePhoneCall({
+      phoneNumber: '0576-88123056', //此号码并非真实电话号码，仅用于测试
+      success: function () {
+        // console.log("拨打电话成功！")
+      },
+      fail: function () {
+        // console.log("拨打电话失败！")
+      }
+    })
+  },
+  calling2: function () {
+    wx.makePhoneCall({
+      phoneNumber: '13666804352', //此号码并非真实电话号码，仅用于测试
+      success: function () {
+        // console.log("拨打电话成功！")
+      },
+      fail: function () {
+        // console.log("拨打电话失败！")
+      }
+    })
   }
 })
